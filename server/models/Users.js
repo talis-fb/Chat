6d37d1b454acc9b7213ad963dbfa38f329a7e73b
talis-fb <talisonf11@gmail.com>
2021-04-05 15:27:32 -0300

    Substituição para o Mongoose na função de Adicionar contatos

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UsersSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  conversations: {
-    //parte dos outros
-  }
+const oneConversation = new Schema({
+	contact: String,
+	type: Number,
+	cod: String
 });
+
+const UsersSchema = new Schema({
+	pin: {
+		type: String,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	conversations: [oneConversation]
+});
+
 
 module.exports = mongoose.model('Users', UsersSchema);
--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.post('/register', async (req, res) => {
 			pin: newPin,
 			name: nickname,
 			password: password,
-			conversation: {}
+			conversation: []
 		})
 
 		const data = await newUser.save()
@@ -77,7 +77,7 @@ app.post('/register', async (req, res) => {
 
 	} catch(err) {
 		console.log(err)
-		return res.status(400).send({ error: err })
+		return res.status(400).send({ registro: false, error: err })
 	}
 
 })
@@ -102,7 +102,8 @@ app.post('/login', async (req, res) => {
 		console.log(UserLogging)
 
 		const dadesToSendBack = {
-			name: UserLogging.name
+			name: UserLogging.name,
+			token: generateAccessToken(UserLogging.name)
 		}
 
 		console.log('LOGADO: '+ UserLogging.name)
@@ -120,43 +121,52 @@ io.on('connection', socket => {
 	
 	//console.log(findUser('Alone'))
 
-    socket.on('addContact', (pin, pinOfSolicitor) => {
+    socket.on('addContact', async (pin, pinOfSolicitor) => {
 
-        const userRequested = (pin!=pinOfSolicitor) && database.users['P'+pin] // Return True if exist a user with the pin received AND the pin been diferent of pinOfSolicitor
+		const peopleInSearch = await UsersDB.findOne({ pin: pin })
+
+        const userRequested = (pin!=pinOfSolicitor) && peopleInSearch // Return True if exist a user with the pin received AND the pin been diferent of pinOfSolicitor
 		
         //If the Pin received dont exist
         if( !userRequested ) return
 
-        const conversationBefore = userRequested.conversations['P'+pinOfSolicitor] // Return pin of conversation (if dont exist a user with will return undefined)
+        const conversationBefore = userRequested.conversations // Return pin of conversation (if dont exist a user with will return undefined)
+
+		console.log('Conversas...')
+		console.log(conversationBefore)
+
         let codeOfConv // Who will receive the code of conversation for acess database and send of user
 
         //CREATE a new Chat (If dont exist a conversation between both users...)
-        if( !conversationBefore ){
+        if( !conversationBefore.length ){
             console.log('CRIA O TALK')
 
             //Creation of code the new conversation
             codeOfConv = Math.random().toString(36).substring(9);
 
             //SET the conversation on database of both users
-            database.users['P'+pin].conversations['P'+pinOfSolicitor] = { 
-                type: 1, //The type is for define from who is the messages
-                c: codeOfConv
-            }
-            database.users['P'+pinOfSolicitor].conversations['P'+pin] = { 
-                type: 2,
-                c: codeOfConv
-            }
+			const user1 = await UsersDB.updateOne( { pin:pin }, {
+				$push: { //Insert a new item in array conversations of user
+					conversations: {
+						contact: 'oi',
+						type: 1,
+						cod: codeOfConv
+					}	
+				}
+			})
+			user1.exec()
 
             //SET in Messages's Database
-            database.messages[codeOfConv] = []
+            //database.messages[codeOfConv] = []
         } else {
             // If it already has a conversation in database
-            codeOfConv = conversationBefore.c
+			console.log('JÁ TEM ESSA CONVERSA PARCEIRO')
+			return
         }
 
         //SENDING BACK for the socket
         const name = userRequested.name
-        const msgs = database.messages[codeOfConv]
+        const msgs = []
         const conversation = {}
         conversation[name] = {
             msgs: msgs
