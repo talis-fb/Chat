c6e1b96cd8032288b82cda5ce7d83b9d322c6055
talis-fb <talisonf11@gmail.com>
2021-04-12 00:40:59 -0300

    Estilização da pagina de login

--- client/src/pages/Home_Page/WelcomeScreen.js
+++ client/src/pages/Home_Page/WelcomeScreen.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import { Redirect, useHistory } from 'react-router-dom'
+import { InputGroup, FormControl, Button, ButtonGroup, ToggleButton } from 'react-bootstrap'
+
+import Svg_1 from '../../assets/group1_.svg'
+import Svg_2 from '../../assets/group2_.svg'
+
+
+const iconUsernameInput = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"><path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>`
+const iconPasswordInput = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16"><path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/></svg>`
 
 import { Alert } from 'react-bootstrap'
 //import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,13 +24,13 @@ export default class WelcomeScreen extends React.Component{
 				password: null,
 				passwordConfirm: null
 			},
-			redirect: null, // <Redirect to="/chat" />
+			redirect: null, 
 			alert_error: null,
-            option: null
+            option: true
             /* 
              * option...
-             * 1 - NEW USER
-             * 2 - LOG IN
+             * true - NEW USER
+             * false - LOG IN
              */
         }
 
@@ -39,19 +47,16 @@ export default class WelcomeScreen extends React.Component{
 
 		let url
 
-		if(this.state.option==1){
+		if( this.state.option ){
 			// REGISTER 
 			url = 'http://localhost:3000/register'
-		} else if(this.state.option==2) {
+		} else {
 			// LOGIN 
 			url = 'http://localhost:3000/login'
-		} else {
-			console.log('OUTRO STATE COMO OPTION')
-			return
-		}
-			
+		}			
+
 		// both passoword space aren't equals
-		if( (passwordConfirm !== password) && this.state.option==1 ){
+		if( (passwordConfirm !== password) && this.state.option==true ){
 			this.setState({ alert_error: 'Senha inseridas diferentes' })
 			return
 		}
@@ -99,50 +104,124 @@ export default class WelcomeScreen extends React.Component{
 		})
 	 }
 
+	returnOption(){
+		if(this.state.option){
+			return (
+				<React.Fragment>
+					
+					<InputGroup className="mb-3 name-log">
+						<InputGroup.Prepend>
+							<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							onChange={this.handleInputChange}
+							name="nickname" 
+							type="text"
+							placeholder="Username"
+							aria-label="Username"
+							aria-describedby="basic-addon1"
+						/>
+					</InputGroup>
 
-    returnOption(){
-        switch(this.state.option){
-            case 1: // Register
-                return (
-                    <React.Fragment>
-                        <input type="text" name="nickname" onChange={this.handleInputChange} className="name-reg" 	   placeholder="Nome Usuario"></input>
-                        <input type="text" name="password" onChange={this.handleInputChange} className="password-reg1" placeholder="Senha"></input>
-                        <input type="text" name="passwordConfirm" onChange={this.handleInputChange} className="password-reg2" placeholder="Repita a senha"></input>
-						{ !!this.state.alert_error ? <Alert className="alert_error" variant="danger">{this.state.alert_error}</Alert> : '' }
-						<button onClick={(event) => this.requestFromServer(event)}>Chat</button>
-                    </React.Fragment>
-                )
-            case 2: // Log In
-                return (
-                    <React.Fragment>
-						<input type="text" name="nickname" onChange={this.handleInputChange} className="name-log" 	  placeholder="Nome"></input>
-                        <input type="text" name="password" onChange={this.handleInputChange} className="password-log" placeholder="Senha"></input>
-						{ !!this.state.alert_error ? <Alert className="alert_error" variant="danger">{this.state.alert_error}</Alert> : '' }
-						<button onClick={(event) => this.requestFromServer(event)}>Chat</button>
-                    </React.Fragment>
-                )
-            default: // Without click
-                return (
-                    <h1>Bem vindo =D</h1>
-                )
-        }
-    }
+					<InputGroup className="mb-3 name-log">
+						<InputGroup.Prepend>
+							<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							onChange={this.handleInputChange}
+							name="password" 
+							type="password"
+							placeholder="Senha"
+							aria-label="Password"
+							aria-describedby="basic-addon1"
+						/>
+					</InputGroup>
 
-    render(){
-        return(
-            <div className="WelcomeScreen">
-                <section className="options-start">
-					<button onClick={() => this.changeOption(1) } >New User</button>
-                    <button onClick={() => this.changeOption(2) } >Log In</button>
-                </section>
-				<form className="forms" >
-                    {this.returnOption()}
 
-					{/*Component that make redirect if the user make login
-						It stays with null value most of time, until the requestFromServer changes its value, receiving response a of server*/}
-					{this.state.redirect}
-                </form>
-            </div>
-        )
-    }
+					<InputGroup className="mb-3 name-log">
+						<InputGroup.Prepend>
+							<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							onChange={this.handleInputChange}
+							name="passwordConfirm" 
+							type="password"
+							placeholder="Repita a senha"
+							aria-label="Username"
+							aria-describedby="basic-addon1"
+						/>
+					</InputGroup>
+
+					{ !!this.state.alert_error ? <Alert className="alert_error" variant="danger">{this.state.alert_error}</Alert> : '' }
+
+
+				</React.Fragment>
+			)
+		} else {
+			return (
+				<React.Fragment>
+
+					<InputGroup className="mb-3 name-log">
+						<InputGroup.Prepend>
+							<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							onChange={this.handleInputChange}
+							name="nickname" 
+							type="text"
+							placeholder="Username"
+							aria-label="Username"
+							aria-describedby="basic-addon1"
+						/>
+					</InputGroup>
+
+
+					<InputGroup className="mb-3 name-log">
+						<InputGroup.Prepend>
+							<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							onChange={this.handleInputChange}
+							name="password" 
+							type="password"
+							placeholder="Password"
+							aria-label="Password"
+							aria-describedby="basic-addon1"
+						/>
+					</InputGroup>
+
+					{ !!this.state.alert_error ? <Alert className="alert_error" variant="danger">{this.state.alert_error}</Alert> : '' }
+
+				</React.Fragment>
+			)
+		}
+	}
+    
+
+	render(){
+
+		console.log(this.state)
+
+		return(
+			<div className="WelcomeScreen">
+				<main className="main">
+					<header className="mb-3">Chat-uTuVisse</header>
+
+					<form className="forms mb-3">
+						{this.returnOption()}
+						<Button className="button-confirm" onClick={(event) => this.requestFromServer(event)} variant="success">CHAT</Button>
+					</form>
+
+					<div className="options">
+						<Button onClick={() => this.changeOption(true) } active={this.state.option}  variant="outline-warning" className="mb-3">Cadastro</Button>
+						<Button  onClick={() => this.changeOption(false) } active={!this.state.option} variant="outline-warning" className="mb-3">Login</Button>
+					</div>
+				</main>
+
+				<section className="art">
+					<img src={Svg_2} />
+				</section>
+			</div>
+		)
+	}
 }
