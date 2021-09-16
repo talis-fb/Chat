5f32fab2e9a144eac41aca00ca198efda738bc0f
talis-fb <talisonf11@gmail.com>
2021-09-15 22:56:17 -0300

    Classe para função: Componentes das mensagens

--- client/src/pages/Chat/Components/Messages/Messages.js
+++ client/src/pages/Chat/Components/Messages/Messages.js
@@ -3,8 +3,14 @@ import React from 'react'
 import Auth from '../../../AuthService'
 import './Messages.scss';
 
-export default class Messages extends React.Component{
-    messageFromUser(msg){
+function Messages(props){
+    // If the chat has the props 'screen' means it is a screen to show
+    // and not a chat. So, it just return the element of that screen
+    if ( props.chat.screen ) return props.chat.element
+
+    const pin = Auth.getCurrentUser().pin
+
+    function messageFromUser(msg){
         return (
             <div className="box-msg">
                 <div className=" msg-User">
@@ -14,7 +20,7 @@ export default class Messages extends React.Component{
         )
     }
 
-    messageFromFriend(msg){
+    function messageFromFriend(msg){
         return (
             <div className="box-msg">
                 <div className=" msg-Friend">
@@ -24,40 +30,36 @@ export default class Messages extends React.Component{
         )
     }
 
-	withoutMessages(){
-		return (
-			<div class="box-msg-empty">
-				<span>CONVERSA VAZIA</span>
-				<span>Puxe um papo!</span>
-			</div>
-		)
-	}
-
-    render(){
-		// If the chat has the props 'screen' means it is a screen to show
-		// and not a chat. So, it just return the element of that screen
-		if ( this.props.chat.screen ) return this.props.chat.element
-
-		const pin = Auth.getCurrentUser().pin
-
-		const messages = this.props.chat.msgs
-		const box_of_messages = messages.map( i => {
-			if ( !i.body ) return null
-			if ( i.from===pin ) return this.messageFromUser(i.body) 
-
-			return this.messageFromFriend(i.body)
-		})
-
-		const withoutMessages = this.withoutMessages()
-		const hasMessages = !!messages.length
-
-        //If it doesn't received messages in props, the 'box_of_messages' keeps null
-        return(
-            <div className="messages-chat">
-				<div className="messages-block">
-				{ hasMessages ? box_of_messages : withoutMessages }
-				</div>
+    function ScreenWithoutMessages(){
+        return (
+            <div class="box-msg-empty">
+                <span>CONVERSA VAZIA</span>
+                <span>Puxe um papo!</span>
             </div>
         )
     }
+
+    // Get the messages received and make an array with component with the messages
+    const messages = props.chat.msgs
+    const box_of_messages = messages.map( i => {
+        if ( !i.body ) return null
+        if ( i.from===pin ) return messageFromUser(i.body) 
+        return messageFromFriend(i.body)
+    })
+    const withoutMessages = ScreenWithoutMessages();
+
+    // Boolean, TRUE -> There are messages, FALSE -> There aren't nothing
+    const hasMessages = !!messages.length
+
+
+    return(
+        <div className="messages-chat">
+            <div className="messages-block">
+                { hasMessages ? box_of_messages : withoutMessages }
+            </div>
+        </div>
+    )
+
 }
+
+export default Messages;
